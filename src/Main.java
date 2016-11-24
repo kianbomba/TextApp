3a9eaae19c856e91949d1342d04b5867c66d716f
kiannguyen <kianbomba@gmail.com>
2016-11-24 21:19:28 +1300

    Upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Operation.App;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        app.run();
+    }
+}
---
+++ src/Operation/Model/Text.java
@@ -0,0 +1,31 @@
+package Operation.Model;
+import java.util.Date;
+public class Text {
+    private String text;
+    private Date Date;
+    private final String author = "Kian Nguyen"; // Please Change the name of the author to your name
+
+    public Text(String text){
+        this.text = text;
+        this.Date = new Date();
+    }
+
+
+    public String getText(){
+        return this.text;
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public Date getlocalDate(){
+        return this.Date;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Message: \t%s \nDate: \t\t%s \nBy: \t\t%s" ,text, Date, author);
+    }
+
+}
